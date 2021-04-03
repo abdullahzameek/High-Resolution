2a8fb69d96483215d6320795a56ec700c53d413a
Abdullah Zameek <arz268@nyu.edu>
2021-04-03 17:13:49 +0400

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import ApplicationRoutes from './ApplicationRoutes';
+import AppBar from './views/components/AppBar';
 
+
+const theme = createMuiTheme({
+    typography: {
+      fontFamily: [
+        'Sk-Modernist-Mono',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif'
+      ].join(','),
+    }
+  });
 
 function App() {
    return (
+    <>
+    <ThemeProvider theme={theme}>
+    <AppBar/>
       <ApplicationRoutes />
+    </ThemeProvider>
+    </>
    );
-}export default App;
+   
+}
+export default App;
