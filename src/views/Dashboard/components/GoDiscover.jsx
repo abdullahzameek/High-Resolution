994e55efb5702ad2c484ca82135204cb947e7f7a
Abdullah Zameek <arz268@nyu.edu>
2021-04-04 05:13:41 +0400

    go to discover

---
+++ src/views/Dashboard/components/GoDiscover.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Redirect } from 'react-router-dom';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function CreateResolution() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+//   const handleClick = () => {
+//     console.log('clcik was triggered here');
+//         <Redirect to={'/createNew'}/>
+//     }   
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          Go to Discover
+        </Typography>
+      </CardContent>
+      <CardActions> 
+        <a href="/discover" size="small">Click Here</a>
+      </CardActions>
+    </Card>
+  );
+}
