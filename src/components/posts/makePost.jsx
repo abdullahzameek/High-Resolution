7c9526a1d42a346b02d25753c7df0e363c67a75f
Filip <bende@localhost.localdomain>
2021-04-04 13:57:07 +0400

    makePost

--- src/components/posts/makePost.jsx
+++ src/components/posts/makePost.jsx
@@ -6,10 +6,24 @@ const storage = firebase.storage().ref();
 export default function MakePost(){
 
     async function onsubmit(e){
+        e?.preventDefault();
+
         const post = e.target.content.value;
         const displayName = sessionStorage.getItem('displayName')
-        const imgref = storage.child(displayName);
-        console.log(e.target.file);
+        const imgref = storage.child(displayName).child(e.target.file.value);
+        imgref.put(e.target.file.files[0]);
+        
+        const resRef = db.collection("Profiles").doc("sexking").collection("Unfinished").doc("53rnAOFd5m1519RxHYcF");
+        const res = await resRef.update(
+            {
+                posts: firebase.firestore.FieldValue.arrayUnion({
+                    img: e.target.file.value,
+                    post: e.target.content.value
+                })
+            }
+        )
+        console.log(e.target.file.value);
+        console.log(e.target.file.files[0]);
     }
 
 
@@ -19,7 +33,8 @@ export default function MakePost(){
                 <label>Post content</label>
                 <textarea id="content"></textarea>
                 <label>postImage</label>
-                <input type="file"></input>
+                <input type="file" id="file"></input>
+                <button>Submit</button>
             </form>
         </div>
     )
